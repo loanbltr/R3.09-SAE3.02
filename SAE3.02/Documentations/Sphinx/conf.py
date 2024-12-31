bb9db574546bdf5e8198f0108815a2d2a5886951
Loan <147603069+loanbltr@users.noreply.github.com>
2024-12-31 18:00:57 +0100

    Update Documentation

---
+++ SAE3.02/Documentations/Sphinx/conf.py
@@ -0,0 +1,31 @@
+import os, sys
+sys.path.insert(0, os.path.abspath(r"C:\Users\loanb\Desktop\SAE3.02\Codes"))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SAE 3.02'
+copyright = '2024, Loan BELTRAN'
+author = 'Loan BELTRAN'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon',"sphinx.ext.viewcode"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
